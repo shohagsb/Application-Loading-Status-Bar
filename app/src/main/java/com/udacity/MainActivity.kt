589aee26bd028480bb1543232c796fa9dc9fec8e
Md Shohag Ali Sarder <shohagsb@gmail.com>
2021-06-09 17:56:51 +0600

    Working on notification

--- app/src/main/java/com/udacity/MainActivity.kt
+++ app/src/main/java/com/udacity/MainActivity.kt
@@ -1,22 +1,27 @@
 package com.udacity
 
 import android.app.DownloadManager
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.graphics.Color
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.widget.RadioButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
+private const val NOTIFICATION_ID = 0
 
 class MainActivity : AppCompatActivity() {
 
@@ -32,13 +37,21 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
+        notificationManager = ContextCompat.getSystemService(
+            applicationContext,
+            NotificationManager::class.java
+        ) as NotificationManager
+
+        createChannel(
+            getString(R.string.download_channel_name)
+        )
 
         custom_button.setOnClickListener {
-            //onRadioButtonClicked(it)
             if (URL != null) {
                 download()
             } else {
-                Toast.makeText(this, "Please select the file to download", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Please select the file to download", Toast.LENGTH_SHORT)
+                    .show()
             }
         }
     }
@@ -46,6 +59,11 @@ class MainActivity : AppCompatActivity() {
     private val receiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
             val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
+            notificationManager.cancelAll()
+            notificationManager.sendNotification(
+                applicationContext.getString(R.string.notification_description),
+                applicationContext
+            )
 
         }
     }
@@ -68,6 +86,61 @@ class MainActivity : AppCompatActivity() {
         private var URL: String? = null
 
         private const val CHANNEL_ID = "channelId"
+    }
+
+    // Create notification Chanel
+    private fun createChannel(channelName: String) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                CHANNEL_ID,
+                channelName,
+                NotificationManager.IMPORTANCE_LOW
+            )
+
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.RED
+            notificationChannel.enableVibration(true)
+            notificationChannel.description = "Time for breakfast"
+
+            val notificationManager = this.getSystemService(
+                NotificationManager::class.java
+            )
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+    }
+
+    //    Send notification extension function
+    fun NotificationManager.sendNotification(messageBody: String, applicationContext: Context) {
+        val contentIntent = Intent(applicationContext, DetailActivity::class.java)
+        pendingIntent = PendingIntent.getActivity(
+            applicationContext,
+            NOTIFICATION_ID,
+            contentIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        // Build the notification
+        val builder = NotificationCompat.Builder(
+            applicationContext,
+            CHANNEL_ID
+        ).setSmallIcon(R.drawable.ic_assistant_black_24dp)
+            .setContentTitle(
+                applicationContext
+                    .getString(R.string.notification_title)
+            )
+            .setContentText(messageBody)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+            .addAction(
+                R.drawable.ic_assistant_black_24dp,
+                applicationContext.getString(R.string.notification_button),
+                pendingIntent
+            )
+
+        notify(NOTIFICATION_ID, builder.build())
+    }
+
+    fun NotificationManager.cancelNotifications() {
+        cancelAll()
     }
 
     fun onRadioButtonClicked(view: View) {
@@ -95,5 +168,6 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
 
 }
